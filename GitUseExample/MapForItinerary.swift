f7a93379aebeaecd064829bfc7f33a65a9a33d03
Vince Day <vince@sch.org>
2018-07-31 12:12:54 -0400

    //commit new branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Vince Day on 7/31/18.
+//  Copyright © 2018 SCH Academy. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
